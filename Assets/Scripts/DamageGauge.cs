7d66647cafc7c661f2b10d9935f4b093513eb202
AdamClaydenUoS <a.clayden@uos.ac.uk>
2024-10-30 17:19:25 +0000

    Overheat system + damage modifiers
    Both are working. Main script is DamageGauge.cs with some placeholder variables within StaticStateValues.cs until we refactor it.
    There's some debugging functions to remove later, but for now the "gauge" is a Slider UI object, and I'm using a C# event to handle its value (increase/decrease/overheat).

---
+++ Assets/Scripts/DamageGauge.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+* Basic overheating system - basic UI that increases or decreases in value based on damage modifiers
+* or from doing nothing. Upon overheat, reset the value back to zero
+*/
+[RequireComponent(typeof(Slider))]
+public class DamageGauge : MonoBehaviour
+{
+    Slider damageGauge;
+    float damageReduction;
+    public delegate void OnMoveDashShoot();
+    public static event OnMoveDashShoot onMoveDashShoot;
+
+    // Handles slider value changes. Functions are currently self contained within this script
+    // move functionality outside of script if/when necessary to link with the player-character
+    public static void RaiseOnMoveDashShoot()
+    {
+        if(onMoveDashShoot != null)
+        {
+            onMoveDashShoot();
+        }
+    }
+
+    /*
+     * Basic overheating system - basic UI that increases or decreases in value based on damage
+     * modifiers or from doing nothing. Upon overheat, reset the value back to zero
+     */
+    // check if we're not moving, shooting or dashing
+
+    #region Unity
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        damageGauge = GetComponent<Slider>();
+        damageReduction = -1f;
+        DebugStates(false); // remove later
+        onMoveDashShoot += IncreaseDamageBasedOnModifiers;
+        onMoveDashShoot += DecreaseDamageIfApplicable;
+        onMoveDashShoot += CheckIfOverheated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DebugOnButtonPress(); // remove later
+        RaiseOnMoveDashShoot();
+    }
+    #endregion
+
+    #region debugging
+
+    /// <summary>
+    /// 1 turns all states off, 2, 3, 4 turns them on
+    /// </summary>
+    void DebugOnButtonPress()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            DebugStates(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            DebugWalkState(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            DebugShootingState(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            DebugDashState(true);
+        }
+    }
+
+    void DebugStates(bool debugValue)
+    {
+        StaticStateValues.isWalking = StaticStateValues.isShooting = StaticStateValues.isDashing = debugValue;
+    }
+
+    void DebugWalkState(bool debugValue)
+    {
+        StaticStateValues.isWalking = debugValue;
+    }
+
+    void DebugShootingState(bool debugValue)
+    {
+        StaticStateValues.isShooting = debugValue;
+    }
+
+    void DebugDashState(bool debugValue)
+    {
+        StaticStateValues.isDashing = debugValue;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Input should come from damage modifier sources as positive or negative values
+    /// Probably some event based system
+    /// </summary>
+    /// <param name="valueToAdjustBy"></param>
+    public void AdjustDamageGaugeValue(float valueToAdjustBy)
+    {
+        damageGauge.value += valueToAdjustBy * Time.deltaTime;
+    }
+
+    void IncreaseDamageBasedOnModifiers()
+    {
+        if(StaticStateValues.isWalking)
+        {
+            AdjustDamageGaugeValue(1f);
+        }
+        if(StaticStateValues.isShooting)
+        {
+            AdjustDamageGaugeValue(1f);
+        }
+        if(StaticStateValues.isDashing)
+        {
+            AdjustDamageGaugeValue(1f);
+        }
+    }
+
+    void CheckIfOverheated()
+    {
+        if (damageGauge.value == 100)
+        {
+            damageGauge.value = 0;
+        }
+    }
+
+    void DecreaseDamageIfApplicable()
+    {
+        if(!StaticStateValues.isWalking && !StaticStateValues.isShooting && !StaticStateValues.isDashing)
+        {
+            AdjustDamageGaugeValue(damageReduction);
+        }
+    }
+}
---
+++ Assets/Scripts/StaticStateValues.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class StaticStateValues : MonoBehaviour
+{
+    public static bool isWalking, isShooting, isDashing;
+}
