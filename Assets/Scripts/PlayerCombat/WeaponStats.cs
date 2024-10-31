b4593a0e1fbc4a81a2f23ba1956754588b33ee41
Chris Janes <gingoreilly@gmail.com>
2024-10-31 13:22:13 +0000

    add weapon stats scriptable object, currently holds base damage and fire delay values.

---
+++ Assets/Scripts/PlayerCombat/WeaponStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace PlayerCombat
+{
+    [CreateAssetMenu]
+    public class WeaponStats : ScriptableObject
+    {
+        public float fireDelay = 0.5f;
+        public float baseDamage = 1.0f;
+    }
+}
