2c9c76d6880aec7e841faee4fa0c1e77fe453f9c
Chris Janes <gingoreilly@gmail.com>
2024-11-01 10:49:25 +0000

    update to the input actions to remove un-necessary inputs and to provide a cleaner approach to differentiating between gamepad / keyboard rotation and mouse based aiming.

--- Assets/Scripts/S_PlayerMovment.cs
+++ Assets/Scripts/S_PlayerMovment.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.Rendering.DebugUI;
 
-
 public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
 {
     [SerializeField] private float speed = 1.0f;
@@ -14,6 +13,7 @@ public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
     {
         anim = gameObject.GetComponentInChildren<Animator>();
         characterRef = gameObject.transform.GetChild(0).gameObject;
+        
     }
 
     public void Update()
@@ -31,19 +31,42 @@ public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
         Vector2 right2d = new Vector2(characterRef.transform.right.x, characterRef.transform.right.z*-1);
         Velocity = context.ReadValue<Vector2>().y * forward2d+ context.ReadValue<Vector2>().x * right2d;
 
-        Debug.Log("forwaed2d:"+forward2d+" Right2d:"+right2d+" Velocity:"+Velocity);
+        //Debug.Log("forwaed2d:"+forward2d+" Right2d:"+right2d+" Velocity:"+Velocity);
     }
 
-    public void OnLook(InputAction.CallbackContext context)
+    // Rotate uses GamePad stick or Keyboard
+    public void OnRotate(InputAction.CallbackContext context)
     {
+        Debug.Log("OnRotate");
         Vector2 direction = context.ReadValue<Vector2>();
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y).normalized, Vector3.up);
         characterRef.transform.rotation = lookRotation;
     }
 
+    // Aim follows the mouse cursor
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnAim");
+        Vector2 direction = context.ReadValue<Vector2>();
+
+        direction = Camera.main.ScreenToViewportPoint(direction);
+        Debug.Log("pointer pos: " + direction);
+        
+        Vector3 lookDirection = new Vector3(direction.x, 0f, direction.y);
+        var lookRotation = Camera.main.transform.TransformDirection(lookDirection);
+        lookRotation = Vector3.ProjectOnPlane(lookRotation, Vector3.up);
+
+        if (lookRotation != Vector3.zero)
+        {
+            //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y).normalized, Vector3.up);
+            Quaternion newRotation = Quaternion.LookRotation(lookRotation);
+            characterRef.transform.rotation = newRotation;
+        }
+    }
+
     public void OnAttack(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+ 
     }
 
     public void OnInteract(InputAction.CallbackContext context)
@@ -57,25 +80,5 @@ public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
         {
             anim.SetTrigger("Roll");
         }
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPrevious(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnNext(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
     }
 }
