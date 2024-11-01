6ca522dad53c1a84a157b4b485bbb5b3009299ba
Chris Janes <gingoreilly@gmail.com>
2024-11-01 14:24:02 +0000

    add aimpoint object to visualise rotation & mouselook direction.
    update to playermovement script to separate mouse look and game stick aiming.
    
    character now rotates in Update rather than in the input event callbacks; there was some weird behaviour if you moved without also "aiming" at the same time otherwise.
    
    TODO: resolve issue with keyboard movement directions being strange.

--- Assets/Scripts/S_PlayerMovment.cs
+++ Assets/Scripts/S_PlayerMovment.cs
@@ -2,33 +2,44 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.Rendering.DebugUI;
 
-public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
+public class S_PlayerMovment: MonoBehaviour
 {
-    [SerializeField] private float speed = 1.0f;
+    //[SerializeField] private float speed = 1.0f;
     private Vector2 Velocity = new();
     private Animator anim;
     private GameObject characterRef;
+
+    public Transform aimPointObject;
+
+    private Vector3 _aimPosition = Vector3.zero;
+    private Transform _characterTransform;
+    private Camera _mainCamera;
+    
+    private static readonly int Roll = Animator.StringToHash("Roll");
+    private static readonly int VelocityX = Animator.StringToHash("VelocityX");
+    private static readonly int VelocityZ = Animator.StringToHash("VelocityZ");
 
     public void Start()
     {
         anim = gameObject.GetComponentInChildren<Animator>();
         characterRef = gameObject.transform.GetChild(0).gameObject;
-        
+        _mainCamera = Camera.main;
+        _characterTransform = characterRef.transform;
     }
 
     public void Update()
     {
-
-
-        anim.SetFloat("VelocityX", Velocity.x);
-        anim.SetFloat("VelocityZ", Velocity.y);
-        
+        anim.SetFloat(VelocityX, Velocity.x);
+        anim.SetFloat(VelocityZ, Velocity.y);
+        _characterTransform.LookAt(_aimPosition);
     }
     
     public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 forward2d = new Vector2(characterRef.transform.forward.x*-1, characterRef.transform.forward.z);
-        Vector2 right2d = new Vector2(characterRef.transform.right.x, characterRef.transform.right.z*-1);
+        var characterForward = _characterTransform.forward;
+        var characterRight = _characterTransform.right;
+        Vector2 forward2d = new Vector2(characterForward.x*-1, characterForward.z);
+        Vector2 right2d = new Vector2(characterRight.x, characterRight.z*-1);
         Velocity = context.ReadValue<Vector2>().y * forward2d+ context.ReadValue<Vector2>().x * right2d;
 
         //Debug.Log("forwaed2d:"+forward2d+" Right2d:"+right2d+" Velocity:"+Velocity);
@@ -37,36 +48,34 @@ public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
     // Rotate uses GamePad stick or Keyboard
     public void OnRotate(InputAction.CallbackContext context)
     {
-        Debug.Log("OnRotate");
+        //Debug.Log("OnRotate");
         Vector2 direction = context.ReadValue<Vector2>();
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y).normalized, Vector3.up);
-        characterRef.transform.rotation = lookRotation;
+
+        if (direction.magnitude <= 0.2f) return;
+        
+        const float dirOffset = 2.0f;
+        
+        Debug.Log(direction);
+        Vector3 offsetPosition = _characterTransform.position + new Vector3(direction.x* dirOffset, 0.0f, direction.y* dirOffset);
+        
+        Vector3 aimPosition = new Vector3((offsetPosition.x + direction.x) * dirOffset, aimPointObject.position.y, (offsetPosition.z + direction.y) * dirOffset);
+        aimPointObject.position = offsetPosition;
+        _aimPosition = offsetPosition;
+        //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y).normalized, Vector3.up);
+
     }
 
     // Aim follows the mouse cursor
     public void OnAim(InputAction.CallbackContext context)
     {
-        Debug.Log("OnAim");
-        Vector2 direction = context.ReadValue<Vector2>();
-
-        direction = Camera.main.ScreenToViewportPoint(direction);
-        Debug.Log("pointer pos: " + direction);
+        Vector2 input = context.ReadValue<Vector2>();
+        Vector3 worldMouse = _mainCamera.ScreenToWorldPoint(new Vector3(input.x, input.y, 10.0f));
+        Vector3 worldMouseNoY = new Vector3(worldMouse.x, _characterTransform.position.y, worldMouse.z);
         
-        Vector3 lookDirection = new Vector3(direction.x, 0f, direction.y);
-        var lookRotation = Camera.main.transform.TransformDirection(lookDirection);
-        lookRotation = Vector3.ProjectOnPlane(lookRotation, Vector3.up);
+        aimPointObject.position = new Vector3(worldMouse.x, aimPointObject.position.y, worldMouse.z);
 
-        if (lookRotation != Vector3.zero)
-        {
-            //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y).normalized, Vector3.up);
-            Quaternion newRotation = Quaternion.LookRotation(lookRotation);
-            characterRef.transform.rotation = newRotation;
-        }
-    }
-
-    public void OnAttack(InputAction.CallbackContext context)
-    {
- 
+        if (worldMouseNoY != Vector3.zero)
+            _aimPosition = worldMouseNoY;
     }
 
     public void OnInteract(InputAction.CallbackContext context)
@@ -78,7 +87,7 @@ public class S_PlayerMovment: MonoBehaviour, InputSystem_Actions.IPlayerActions
     {
         if (context.phase == InputActionPhase.Started)
         {
-            anim.SetTrigger("Roll");
+            anim.SetTrigger(Roll);
         }
     }
 }
