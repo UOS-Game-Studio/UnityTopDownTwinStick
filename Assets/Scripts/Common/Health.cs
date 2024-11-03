9e236051175cbc910710e49bc1c80e1656a2d2a0
Chris Janes <gingoreilly@gmail.com>
2024-11-03 18:03:54 +0000

    add Health component
    update Monster and Player prefabs with new Health component
    
    add EnemyCore as stub, added to Monster prefab
    
    add Capsule Collider to Player Prefab

---
+++ Assets/Scripts/Common/Health.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Common
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private float maxHealth = 1.0f;
+        private readonly UnityEvent<Health> _onDeath = new UnityEvent<Health>();
+        
+        public void TakeDamage(float damage)
+        {
+            maxHealth = Mathf.Min(0.0f, maxHealth - damage);
+
+            // trigger some vfx or similar to indicate the hit
+            
+            if (maxHealth > 0.0f) return;
+            
+            _onDeath.Invoke(this);
+            Destroy(gameObject, 0.1f);
+        }
+
+        private void OnDestroy()
+        {
+            _onDeath.RemoveAllListeners();
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            GameController gameController = GameObject.FindAnyObjectByType<GameController>();
+
+            _onDeath.AddListener(gameController.OnCharacterKilled);
+        }
+    }
+
+}
